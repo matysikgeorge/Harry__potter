1da3acdb5e3181d91fb3c1268b2937873f394f0a
matysikgeorge <matysikgeorge1995@gmail.com>
2024-08-29 17:05:07 +0300

    first commit

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,51 +1,74 @@
 let data = [];
 
-function getData() {
-  fetch("https://harry-potter-api-3a23c827ee69.herokuapp.com/api/characters")
-    .then((response) => response.json())
-    .then((response) => cardRender(response));
+//- функция
+async function getData() {
+  try {
+    const API = await fetch(
+      "https://harry-potter-api-3a23c827ee69.herokuapp.com/api/characters"
+    );
+    data = await API.json();
+    cardRender(data);
+  } catch (err) {
+    console.log(err);
+  }
 }
-getData() // - вызов функции 
+getData(); // - вызов функции
 
 // import data from "./json/characters.json" with {type: "json"}
 // console.log(data);
 
-const cardsSectionNode = document.querySelector(".cards__section");
-
+const cardsSectionNode = document.querySelector(".cards__wrapper");
 
 // - создание шаблона для карточек актеров
 function createHeroCard(array) {
-  console.log(array);
-  
-  const { actor, alive, gender, house, image, name, wand:{core}} = array;
-  const heroCard = document.createElement("div");
-  heroCard.className = "cards__wrapper";
+  const {
+    actor,
+    alive,
+    gender,
+    house,
+    image,
+    name,
+    wand: { core },
+  } = array; //- деструкторизация
+  const heroCard = document.createElement("article");
+  heroCard.className = "card";
   heroCard.innerHTML = `
-  <article class="cards">
   <img
-                  class="cards__img"
+                  class="card__img"
                   src=${image}
                 />
-                <div class="cards__text-block">
-                  <h2 class="cards__hero"> ${name}</h2>
-                  <p class="cards__name">Actor: ${actor}</p>
-                  <p class="cards__gender">Gender: ${gender}</p>
-                  <p class="cards__faculty">House: ${house}</p>
-                  <p class="cards__wand">Wand core: ${core}</p>
-                  <p class="cards__life">Alive: ${alive}</p>
+                <div class="card__text-block">
+                  <h2 class="card__hero"> ${name}</h2>
+                  <p class="card__name">Actor: ${actor}</p>
+                  <p class="card__gender">Gender: ${gender}</p>
+                  <p class="card__faculty">House: ${house}</p>
+                  <p class="card__wand">Wand core: ${core}</p>
+                  <p class="card__life">Alive: ${alive}</p>
                 </div>
-                </article>
   `;
   return heroCard;
 }
 
 // - функция отрисовки карточек
-function cardRender(data) {
-  cardsSectionNode.innerHTML = "";
+function cardRender(data) {  
+  cardsSectionNode.innerHTML = '';
   data.forEach((element) => {
     const heroCard = createHeroCard(element);
     cardsSectionNode.append(heroCard);
   });
 }
+
+const inputNode = document.querySelector(".form__input__name");
+
+// - функция поиска по героям
+function cardFind(str) {
+  // let result = event.target.value.toLowerCase().trim();  
+  let newData = data.filter((el) =>
+    el.name.toLowerCase().includes(str.toLowerCase())
+  );
+  cardRender(newData);
+}
+
+inputNode.addEventListener("input", (event) => cardFind(event.target.value.trim()));
 
 // верстку доделать карточки, и поиск по людям,
